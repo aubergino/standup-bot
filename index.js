b716c5d05ea501a8fba519ac6b5545bf72783105
Aidan Coombs <104541011@student.swin.edu.au>
2025-09-22 15:22:09 +1000

    Update for modern use

--- index.js
+++ index.js
@@ -21,7 +21,6 @@ const PREFIX = "!";
 const standupIntroMessage = new MessageEmbed()
   .setColor("#ff9900")
   .setTitle("Daily Standup")
-  .setURL("https://www.youtube.com/watch?v=dQw4w9WgXcQ")
   .setDescription(
     "This is the newly generated text channel used for daily standups! :tada:"
   )
@@ -72,11 +71,7 @@ for (const file of commandFiles) {
 
 // mongodb setup with mongoose
 mongoose
-  .connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useUnifiedTopology: true,
-  })
+  .connect(process.env.MONGO_URI)
   .catch((e) => console.error(e));
 
 mongoose.connection.once("open", () => console.log("mongoDB connected"));
